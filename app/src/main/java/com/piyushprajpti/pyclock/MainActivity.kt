e5c2fdab6eb31dca65a3fa338a187e2f4c50634b
Piyush Prajapati <piyushoa2004@gmail.com>
2024-08-26 15:18:33 +0530

    added logic to take notification permission from user

--- app/src/main/java/com/piyushprajpti/pyclock/MainActivity.kt
+++ app/src/main/java/com/piyushprajpti/pyclock/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.piyushprajpti.pyclock
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.intPreferencesKey
@@ -29,6 +40,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
+            // app theme logics
             val configuration = LocalConfiguration.current
             val uiMode = configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
 
@@ -50,6 +62,34 @@ class MainActivity : ComponentActivity() {
                 2 -> false
                 3 -> true
                 else -> (uiMode == Configuration.UI_MODE_NIGHT_YES)
+            }
+
+            // notification logics
+            val context = LocalContext.current
+
+            var hasNotificationPermission by remember {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    mutableStateOf(
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.POST_NOTIFICATIONS
+                        ) == PackageManager.PERMISSION_GRANTED
+                    )
+                } else mutableStateOf(true)
+            }
+
+
+            val permissionLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission(),
+                onResult = { isGranted ->
+                    hasNotificationPermission = isGranted
+                }
+            )
+
+            SideEffect {
+                if (!hasNotificationPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
             }
 
             PY_ClockTheme(darkTheme = isDarkTheme) {
--- app/src/main/java/com/piyushprajpti/pyclock/RootApplication.kt
+++ app/src/main/java/com/piyushprajpti/pyclock/RootApplication.kt
@@ -1,7 +1,24 @@
 package com.piyushprajpti.pyclock
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RootApplication: Application()
+class RootApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "id",
+                "Notifications",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
