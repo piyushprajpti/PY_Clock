8dc5714d84d2ec3774de3652e5050608f8a864ad
Piyush Prajapati <piyushoa2004@gmail.com>
2024-09-09 18:16:18 +0530

    added no alarm icon for AlarmScreen.kt

--- app/src/main/java/com/piyushprajpti/pyclock/presentation/alarm_screen/AlarmScreen.kt
+++ app/src/main/java/com/piyushprajpti/pyclock/presentation/alarm_screen/AlarmScreen.kt
@@ -3,7 +3,11 @@ package com.piyushprajpti.pyclock.presentation.alarm_screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,6 +16,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -20,8 +26,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.piyushprajpti.pyclock.R
 import com.piyushprajpti.pyclock.data.local_storage.alarm.AlarmData
 import com.piyushprajpti.pyclock.presentation.alarm_screen.util.AlarmCard
 import com.piyushprajpti.pyclock.presentation.alarm_screen.util.AlarmViewModel
@@ -42,8 +51,36 @@ fun AlarmScreen(
 
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(alarmsList.value) { alarmData ->
-                AlarmCard(alarmData = alarmData, onClick = { onAlarmCardClick(alarmData.id) })
+
+            if (alarmsList.value.isEmpty()) {
+                item {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 150.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_no_alarms),
+                                contentDescription = "no alarms",
+                                tint = MaterialTheme.colorScheme.secondary,
+                                modifier = Modifier.size(100.dp)
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "No alarms",
+                                color = MaterialTheme.colorScheme.secondary,
+                                style = MaterialTheme.typography.bodyLarge,
+                                fontSize = 20.sp
+                            )
+                        }
+                    }
+                }
+            } else {
+                items(alarmsList.value) { alarmData ->
+                    AlarmCard(alarmData = alarmData, onClick = { onAlarmCardClick(alarmData.id) })
+                }
             }
         }
 
