63fa4fdeb1c430c8c558c11ddfe8ad210830fb1f
Piyush Prajapati <piyushoa2004@gmail.com>
2024-09-19 13:21:24 +0530

    modified dialog box to adapt orientation changes

--- app/src/main/java/com/piyushprajpti/pyclock/util/DialogBox.kt
+++ app/src/main/java/com/piyushprajpti/pyclock/util/DialogBox.kt
@@ -2,13 +2,13 @@ package com.piyushprajpti.pyclock.util
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -27,43 +28,54 @@ fun DialogBox(
     content: @Composable (() -> Unit),
     onCancelClick: () -> Unit,
     onOkClick: () -> Unit,
-    usePlatformDefaultWidth: Boolean = true
+    boxWidth: Float
 ) {
     Dialog(
         onDismissRequest = { onCancelClick() },
-        properties = DialogProperties(usePlatformDefaultWidth = usePlatformDefaultWidth)
-        ) {
-        Column(
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        LazyColumn(
             modifier = Modifier
+                .fillMaxWidth(boxWidth)
                 .background(MaterialTheme.colorScheme.background, RoundedCornerShape(16.dp))
-                .padding(horizontal = if (!usePlatformDefaultWidth) 10.dp else 20.dp, vertical = 20.dp)
+                .padding(
+                    horizontal = if (boxWidth == 1f) 10.dp else 20.dp,
+                    vertical = 20.dp
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (title != null) {
-                Text(
-                    text = title.toString(),
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.primary,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
+            item {
+                if (title != null) {
+                    Text(
+                        text = title.toString(),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.fillMaxWidth()
+                    )
 
-                Spacer(modifier = Modifier.height(20.dp))
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
             }
 
-            content()
+            item { content() }
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
 
-                DialogActionButton(title = "CANCEL", onClick = { onCancelClick() })
+                    DialogActionButton(title = "CANCEL", onClick = { onCancelClick() })
 
-                Spacer(modifier = Modifier.width(20.dp))
+                    Spacer(modifier = Modifier.width(20.dp))
 
-                DialogActionButton(title = "OK", onClick = { onOkClick() })
+                    DialogActionButton(title = "OK", onClick = { onOkClick() })
 
+                }
             }
         }
     }
