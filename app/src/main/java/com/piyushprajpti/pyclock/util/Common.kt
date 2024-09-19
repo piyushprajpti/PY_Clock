056f6c156b1697b58edf8fdb7996bb84bfc8e342
Piyush Prajapati <piyushoa2004@gmail.com>
2024-09-19 13:36:17 +0530

    created an extension to fix text size

--- app/src/main/java/com/piyushprajpti/pyclock/util/Common.kt
+++ app/src/main/java/com/piyushprajpti/pyclock/util/Common.kt
@@ -2,6 +2,10 @@ package com.piyushprajpti.pyclock.util
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.piyushprajpti.pyclock.presentation.alarm_screen.util.convertMillisInRemainingTime
 
 fun formatTime(hours: String, minutes:String, seconds: String): String {
@@ -11,6 +15,10 @@ fun formatTime(hours: String, minutes:String, seconds: String): String {
 fun Int.pad(): String {
     return this.toString().padStart(2, '0')
 }
+
+val TextUnit.fix
+    @Composable
+    get() = (this.value / LocalDensity.current.fontScale).sp
 
 fun toastRemainingTime(time: Long, context: Context) {
     val message = convertMillisInRemainingTime(time)
