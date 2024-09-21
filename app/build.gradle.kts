6ddbe01aa12bc6bc845445a0b21a609ec29ded2b
Piyush Prajapati <piyushoa2004@gmail.com>
2024-09-21 17:14:56 +0530

    uploaded new 1.3 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.piyushprajpti.pyclock"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
